f2d52225fa59c93c3a9c51960ce8b7d05affdc59
Sweta <36526761+Sweta1012@users.noreply.github.com>
2019-04-16 18:03:18 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// add this file to .gitignore
-
-module.exports = {
-    google: {
-        clientID: '109263375074-s8tkil6aliq756hv28cuqrjp4er22b5o.apps.googleusercontent.com',
-        clientSecret:'mG5biddW7LX5AmSBszUk9c0X'
-    }
-}
