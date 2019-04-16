58961bb149e285984263735f915187fab19d4434
Sweta <swetap1012@gmail.com>
2019-04-16 15:07:54 -0400

    OAuth- Passport.js project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+//set up view engine
+
+app.set('view engine', 'ejs');
+
+//create home route
+
+app.get('/', (req, res) => {
+    res.render('home'); //render the home template from views
+});
+
+app.listen(3000, () => {
+    console.log("app listening on port 3000");
+});
